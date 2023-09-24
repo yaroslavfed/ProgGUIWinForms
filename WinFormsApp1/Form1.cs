2745a18b4707bbc47aff39dcceebe3f5dcd6ded5
yaroslavfed <yaroslav.site@mail.ru>
2023-09-25 01:53:57 +0700

    Revert "Опять в мастер ветвь"
    This reverts commit 40121936026071f69afbc93cce2c39201b4a1272.

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -61,6 +61,7 @@ namespace WinFormsApp1
         private void button3_Click(object sender, EventArgs e)
         {
             // TODO: доделать вызов ивента на текущее состояние
+            // Вызов действий от стейт машины
         }
     }
     public class Data
