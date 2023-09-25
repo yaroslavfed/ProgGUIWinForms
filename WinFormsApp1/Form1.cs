9dacebcfba3ad8b708fd14af1b613623ea00a640
yaroslavfed <yaroslav.site@mail.ru>
2023-09-26 03:40:36 +0700

    Доработана стейт машина и разделены сборки Принимает состояние и вызывает ивенты в зависимости от него Разделена сборка для задания 2 и 3 При выборе сборки ReleaseAutoBinding автоматически применяются изменения из таблицы А так же при смене спрайта

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,28 +2,39 @@ using System.Windows.Forms;
 using System;
 using System.Runtime.InteropServices;
 using Appccelerate.StateMachine;
+using System.Reflection.Emit;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-        private SimpleStateMachine? _fsm;
-        private EState? _currentState;
-
         public BindingSource Data { get; set; }
+
         public Form1()
         {
             Data = new BindingSource();
             Data.Add(new Data() { X = 0, Y = 0 });
             Data.Add(new Data() { X = 1, Y = 1 });
             Data.Add(new Data() { X = 2, Y = 4 });
+            InitializeFsm();
             InitializeComponent();
             dataGridView1.DataSource = Data;
+            comboBox1.Items.AddRange(new object[]
+            {
+                defaultTextLabel,
+                "Draw as lines",
+                "Draw as spline"
+            });
+            comboBox1.SelectedIndex = 0;
         }
+
+        private string defaultTextLabel = "Select draw sprite";
+
         private void AddButton_Click(object sender, EventArgs e)
         {
             Data.Add(new Data() { X = 4, Y = 16 });
         }
+
         private void DrawAsLines_Click(object sender, EventArgs e)
         {
             chart1.DataSource = null;
@@ -31,6 +42,7 @@ namespace WinFormsApp1
                 System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             chart1.DataSource = Data;
         }
+
         private void DrawAsSpline_Click(object sender, EventArgs e)
         {
             chart1.DataSource = null;
@@ -39,29 +51,27 @@ namespace WinFormsApp1
             chart1.DataSource = Data;
         }
 
-        private int _actionIndex;
-
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            _actionIndex = comboBox1.SelectedIndex;
-            switch (_actionIndex)
-            {
-                case 0:
-                    _currentState = EState.Line;
-                    break;
-                case 1:
-                    _currentState = EState.Spline;
-                    break;
-                default:
-                    _currentState = EState.Line;
-                    break;
-            }
+            _targetState = _clientTargetList[comboBox1.SelectedIndex];
+
+            _fsm?.Fire(EEvent.ChangeDrawSprite);
+
+#if AutoBinding
+            _fsm?.Fire(EEvent.DrawEvent); 
+#endif
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            // TODO: доделать вызов ивента на текущее состояние
-            // Вызов действий от стейт машины
+            _fsm?.Fire(EEvent.DrawEvent);
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, EventArgs e)
+        {
+#if AutoBinding
+            _fsm?.Fire(EEvent.DrawEvent); 
+#endif
         }
     }
     public class Data
--- WinFormsApp1/SimpleStateMachine.cs
+++ WinFormsApp1/SimpleStateMachine.cs
@@ -3,49 +3,120 @@ using Appccelerate.StateMachine.Machine;
 
 namespace WinFormsApp1
 {
-    public class SimpleStateMachine
+    public enum EEvent
     {
-        private readonly PassiveStateMachine<EState, EEvent> machine;
+        DrawEvent,
+        ChangeDrawSprite
+    }
 
-        public SimpleStateMachine()
+    public enum EContextStates
+    {
+        DefaultState,
+        LineState,
+        SplineState
+    }
+
+    public partial class Form1
+    {
+        #region Private
+
+        private EContextStates _currentState = EContextStates.DefaultState;
+        private EContextStates _targetState = EContextStates.DefaultState;
+
+        private PassiveStateMachine<EContextStates, EEvent>? _fsm;
+        private IReadOnlyList<EContextStates> _clientTargetList = new List<EContextStates>()
         {
-            var builder = new StateMachineDefinitionBuilder<EState, EEvent>();
+            EContextStates.DefaultState,
+            EContextStates.LineState,
+            EContextStates.SplineState
+        };
 
-            builder.In(EState.Line)
+        #endregion
+
+        #region Constructors
+
+        private void InitializeFsm()
+        {
+            var builder = new StateMachineDefinitionBuilder<EContextStates, EEvent>();
+
+            builder.In(EContextStates.DefaultState)
                 .On(EEvent.ChangeDrawSprite)
-                    .Goto(EState.Spline)
-                    .Execute(DrawAsSpline)
-                .On(EEvent.Draw)
+                    .If(() => _targetState == EContextStates.LineState).Goto(EContextStates.LineState)
+                    .If(() => _targetState == EContextStates.SplineState).Goto(EContextStates.SplineState)
+                .On(EEvent.DrawEvent)
+                    .Goto(EContextStates.LineState)
+                    .Execute(ErrorDraw);
+
+            builder.In(EContextStates.LineState)
+                .On(EEvent.ChangeDrawSprite)
+                    .If(() => _targetState == EContextStates.DefaultState).Goto(EContextStates.DefaultState)
+                    .If(() => _targetState == EContextStates.SplineState).Goto(EContextStates.SplineState)
+                .On(EEvent.DrawEvent)
                     .Execute(DrawAsLine);
 
-            builder.In(EState.Spline)
+            builder.In(EContextStates.SplineState)
                 .On(EEvent.ChangeDrawSprite)
-                    .Goto(EState.Line)
-                    .Execute(DrawAsLine)
-                .On(EEvent.Draw)
+                    .If(() => _targetState == EContextStates.DefaultState).Goto(EContextStates.DefaultState)
+                    .If(() => _targetState == EContextStates.LineState).Goto(EContextStates.LineState)
+                .On(EEvent.DrawEvent)
                     .Execute(DrawAsSpline);
 
-            builder.WithInitialState(EState.Line);
+#if false
+            builder.In(EContextStates.DefaultState).ExecuteOnEntry(() =>
+            {
+                _currentState = EContextStates.DefaultState;
+            });
+
+            builder.In(EContextStates.LineState).ExecuteOnEntry(() =>
+            {
+                _currentState = EContextStates.LineState;
+            });
+
+            builder.In(EContextStates.SplineState).ExecuteOnEntry(() =>
+            {
+                _currentState = EContextStates.SplineState;
+            });  
+#endif
+
+            builder.WithInitialState(EContextStates.DefaultState);
 
             var definition = builder
                 .Build();
 
-            machine = definition
+            _fsm = definition
                 .CreatePassiveStateMachine("SimpleStateMachine");
 
-            machine.Start();
+            _fsm.Start();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void DrawAsLine()
+        {
+            chart1.DataSource = null;
+            chart1.Series[0].ChartType =
+                System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.DataSource = Data;
         }
 
         private void DrawAsSpline()
         {
-            // TODO: закинуть сюда структуру аналогичную нажатию на кнопку
-            MessageBox.Show("DrawAsSpline");
+            chart1.DataSource = null;
+            chart1.Series[0].ChartType =
+                System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.DataSource = Data;
         }
 
-        private void DrawAsLine()
+        private void ErrorDraw()
         {
-            // TODO: закинуть сюда структуру аналогичную нажатию на кнопку
-            MessageBox.Show("DrawAsSpline");
+#if !AutoBinding
+            MessageBox.Show("Auto selected line sprite");
+#endif
+            DrawAsLine();
         }
+
+        #endregion
     }
 }
